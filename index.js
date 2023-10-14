ecf9c43edd770307488ce05ae3fd7905f05f482c
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-10-14 22:59:49 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    
+    query = query.toLowerCase();
+  
+   
+    return drivers.filter(driver => driver.toLowerCase() === query);
+  }
+
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+
+   
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
